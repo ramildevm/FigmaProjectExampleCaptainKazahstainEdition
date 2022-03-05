a576d9234be4fe43d3643ffb06d398d50ddd4b47
ramildevm <ramil.rahimov2003@gmail.com>
2022-03-05 22:24:44 +0300

    Добавлена анимация движения imageView

--- app/src/main/java/com/example/figmaprojectexamplecaptainkazahstainedition/MainActivity.java
+++ app/src/main/java/com/example/figmaprojectexamplecaptainkazahstainedition/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.figmaprojectexamplecaptainkazahstainedition;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,8 +16,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.buttonGo2Page).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Bundle bundle = null;
+                View view = findViewById(R.id.imgGoHome);
+                String str = getString(R.string.image_transition);
+                if (v != null) {
+                    ActivityOptions options;
+                    options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this, view, str);
+                    bundle = options.toBundle();
+                }
                 Intent switcher = new Intent(MainActivity.this, SecondPageOfCKP.class);
-                MainActivity.this.startActivity(switcher);
+                MainActivity.this.startActivity(switcher, bundle);
                 overridePendingTransition(R.anim.alpha_in,R.anim.alpha_out);
             }
         });
